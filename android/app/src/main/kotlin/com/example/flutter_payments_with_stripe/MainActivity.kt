155eb08075f4df46fb9ac159fa8a4b2f2dc85dde
Nikola Brodar <nikola.brodar@cinnamon.agency>
2024-03-30 21:01:17 -0700

    First commit

---
+++ android/app/src/main/kotlin/com/example/flutter_payments_with_stripe/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.flutter_payments_with_stripe
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
